529c2c0c6e78d6a0802f31e826719f326f551378
Rafa Mossetto <remossetto@teco.com.ar>
2024-07-09 00:07:42 -0300

    v1.18.1: fix secretKey pass in login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
   async login(user: UserDocument) {
     const payload = { username: user.username, sub: user._id };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, { secret: process.env.JWT_SECRET }),
     };
   }
 
